6ef8c2bdfd43cfbbd16166233b656245681648e7
lanfog <lanfog@qq.com>
2020-08-11 09:15:04 +0800

    update

--- src/main/java/cc/mrbird/febs/appmedical/entity/AliYun.java
+++ src/main/java/cc/mrbird/febs/appmedical/entity/AliYun.java
@@ -14,8 +14,8 @@ public class AliYun {
 
     public static void note(String phone,String code ) throws Exception{
 
-        final String SignName = "忙碌与责任直到这时这刻";
-        final String templateCode="SMS_188550946";
+        final String SignName = "";
+        final String templateCode="";
 
         //设置超时时间-可自行调整
         System.setProperty("sun.net.client.defaultConnectTimeout", "10000");
@@ -24,8 +24,8 @@ public class AliYun {
         final String product = "Dysmsapi";//短信API产品名称（短信产品名固定，无需修改）
         final String domain = "dysmsapi.aliyuncs.com";//短信API产品域名（接口地址固定，无需修改）
             //替换成你的AK
-        final String accessKeyId = "LTAI4G7rLdvL87jbtRhWaEFC";//你的accessKeyId,参考本文档步骤2
-        final String accessKeySecret = "dPKhqQU6C2JCVYYjBQ5lQMTBk82g6t";//你的accessKeySecret，参考本文档步骤2
+        final String accessKeyId = "";//你的accessKeyId,参考本文档步骤2
+        final String accessKeySecret = "";//你的accessKeySecret，参考本文档步骤2
         //初始化ascClient,暂时不支持多region（请勿修改）
         IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId,
                 accessKeySecret);
